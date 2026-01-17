96dd981be54ee0331e4a03e757cd0e5c85f40860
samuko-things <samuel.c.agba@gmail.com>
2026-01-17 14:54:29 +0100

    update serial communication

--- eimu.py
+++ eimu.py
@@ -1,30 +1,53 @@
 import serial
 import struct
 
-class EIMUSerialError(Exception):
-    """Custom exception for for EIMU Comm failure"""
-    pass
+# class EIMUSerialError(Exception):
+#     """Custom exception for for EIMU Comm failure"""
+#     pass
 
 START_BYTE = 0xBB
 READ_QUAT = 0x01
 READ_RPY = 0x02
 READ_RPY_VAR = 0x03
+WRITE_RPY_VAR = 0x04
 READ_ACC = 0x05
+READ_ACC_RAW = 0x06
+READ_ACC_OFF = 0x07
+WRITE_ACC_OFF = 0x08
 READ_ACC_VAR = 0x09
+WRITE_ACC_VAR = 0x0A
 READ_GYRO = 0x0B
+READ_GYRO_RAW = 0x0C
+READ_GYRO_OFF = 0x0D
+WRITE_GYRO_OFF = 0x0E
 READ_GYRO_VAR = 0x0F
+WRITE_GYRO_VAR = 0x10
 READ_MAG = 0x11
+READ_MAG_RAW = 0x12
+READ_MAG_H_OFF = 0x13
+WRITE_MAG_H_OFF = 0x14
+READ_MAG_S_OFF0 = 0x15
+WRITE_MAG_S_OFF0 = 0x16
+READ_MAG_S_OFF1 = 0x17
+WRITE_MAG_S_OFF1 = 0x18
+READ_MAG_S_OFF2 = 0x19
+WRITE_MAG_S_OFF2 = 0x1A
+SET_I2C_ADDR = 0x1B
+GET_I2C_ADDR = 0x1C
 SET_FILTER_GAIN = 0x1D
 GET_FILTER_GAIN = 0x1E
 SET_FRAME_ID = 0x1F
 GET_FRAME_ID = 0x20
+RESET_PARAMS = 0x21
 READ_QUAT_RPY = 0x22
 READ_ACC_GYRO = 0x23
 CLEAR_DATA_BUFFER = 0x27
 READ_IMU_DATA = 0x28
+SET_ACC_LPF_CUT_FREQ = 0x29
+GET_ACC_LPF_CUT_FREQ = 0x2A
 READ_LIN_ACC_RAW = 0x2B
 READ_LIN_ACC = 0x2C
-
+#---------------------------------------------
 
 
 class EIMU:
@@ -194,9 +217,6 @@ class EIMU:
         success, frame_id = self.read_data1(GET_FRAME_ID)
         return success, int(frame_id)
     
-    def setFilterGain(self, gain):
-        self.write_data1(SET_FILTER_GAIN, gain)
-
     def getFilterGain(self):
         success, gain = self.read_data1(GET_FILTER_GAIN)
         return success, round(gain, 3)
@@ -213,9 +233,9 @@ class EIMU:
         success, r, p, y = self.read_data3(READ_RPY_VAR)
         return success, round(r, 6), round(p, 6), round(y, 6)
     
-    # def readAcc(self):
-    #     success, ax, ay, az = self.read_data3(READ_ACC)
-    #     return success, round(ax, 6), round(ay, 6), round(az, 6)
+    def readAcc(self):
+        success, ax, ay, az = self.read_data3(READ_ACC)
+        return success, round(ax, 6), round(ay, 6), round(az, 6)
     
     def readAccVariance(self):
         success, ax, ay, az = self.read_data3(READ_ACC_VAR)
@@ -229,9 +249,9 @@ class EIMU:
         success, gx, gy, gz = self.read_data3(READ_GYRO_VAR)
         return success, round(gx, 6), round(gy, 6), round(gz, 6)
     
-    # def readMag(self):
-    #     success, mx, my, mz = self.read_data3(READ_MAG)
-    #     return success, round(mx, 6), round(my, 6), round(mz, 6)
+    def readMag(self):
+        success, mx, my, mz = self.read_data3(READ_MAG)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
     
     def readAccGyro(self):
         success, ax, ay, az, gx, gy, gz = self.read_data6(READ_ACC_GYRO)
@@ -248,4 +268,90 @@ class EIMU:
     def readLinearAcc(self):
         success, ax, ay, az = self.read_data3(READ_LIN_ACC)
         return success, round(ax, 6), round(ay, 6), round(az, 6)
+    #---------------------------------------------------------------------
+
+    def setI2cAddress(self, i2cAddress):
+        self.write_data1(SET_I2C_ADDR, i2cAddress)
+    
+    def getI2cAddress(self):
+        success, i2cAddress = self.read_data1(GET_I2C_ADDR)
+        return success, int(i2cAddress)
+    
+    def resetAllParams(self):
+        success, _ = self.read_data1(RESET_PARAMS)
+        return success
+
+    def setFilterGain(self, gain):
+        self.write_data1(SET_FILTER_GAIN, gain)
+    
+    def setAccFilterCF(self, cf):
+        self.write_data1(SET_ACC_LPF_CUT_FREQ, cf)
+    
+    def getAccFilterCF(self):
+        success, cf = self.read_data1(GET_ACC_LPF_CUT_FREQ)
+        return success, round(cf, 3)
+    
+    def writeRPYVariance(self, r, p, y):
+        self.write_data3(WRITE_RPY_VAR, r, p, y)
+    
+    def readAccRaw(self):
+        success, ax, ay, az = self.read_data3(READ_ACC_RAW)
+        return success, round(ax, 6), round(ay, 6), round(az, 6)
+    
+    def readAccOffset(self):
+        success, ax, ay, az = self.read_data3(READ_ACC_OFF)
+        return success, round(ax, 6), round(ay, 6), round(az, 6)
+    
+    def writeAccOffset(self, ax, ay, az):
+        self.write_data3(WRITE_ACC_OFF, ax, ay, az)
+    
+    def writeAccVariance(self, ax, ay, az):
+        self.write_data3(WRITE_ACC_VAR, ax, ay, az)
+    
+    def readGyroRaw(self):
+        success, gx, gy, gz = self.read_data3(READ_GYRO_RAW)
+        return success, round(gx, 6), round(gy, 6), round(gz, 6)
+    
+    def readGyroOffset(self):
+        success, gx, gy, gz = self.read_data3(READ_GYRO_OFF)
+        return success, round(gx, 6), round(gy, 6), round(gz, 6)
+    
+    def writeGyroOffset(self, gx, gy, gz):
+        self.write_data3(WRITE_GYRO_OFF, gx, gy, gz)
+    
+    def writeGyroVariance(self, gx, gy, gz):
+        self.write_data3(WRITE_GYRO_VAR, gx, gy, gz)
+    
+    def readMagRaw(self):
+        success, mx, my, mz = self.read_data3(READ_MAG_RAW)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
+    
+    def readMagHardOffset(self):
+        success, mx, my, mz = self.read_data3(READ_MAG_H_OFF)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
+    
+    def writeMagHardOffset(self, mx, my, mz):
+        self.write_data3(WRITE_MAG_H_OFF, mx, my, mz)
+    
+    def readMagSoftOffset0(self):
+        success, mx, my, mz = self.read_data3(READ_MAG_S_OFF0)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
+    
+    def writeMagSoftOffset0(self, mx, my, mz):
+        self.write_data3(WRITE_MAG_S_OFF0, mx, my, mz)
+    
+    def readMagSoftOffset1(self):
+        success, mx, my, mz = self.read_data3(READ_MAG_S_OFF1)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
+    
+    def writeMagSoftOffset1(self, mx, my, mz):
+        self.write_data3(WRITE_MAG_S_OFF1, mx, my, mz)
+    
+    def readMagSoftOffset2(self):
+        success, mx, my, mz = self.read_data3(READ_MAG_S_OFF2)
+        return success, round(mx, 6), round(my, 6), round(mz, 6)
+    
+    def writeMagSoftOffset2(self, mx, my, mz):
+        self.write_data3(WRITE_MAG_S_OFF2, mx, my, mz)
+    
     #---------------------------------------------------------------------
